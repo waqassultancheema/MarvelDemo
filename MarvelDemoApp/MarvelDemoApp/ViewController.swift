87bb6c8ec9d8d9062c25aef68b6286edfa93c745
wasultan <wa.sultan@mohre.gov.ae>
2018-05-20 09:17:30 +0400

    First Commit

---
+++ MarvelDemoApp/MarvelDemoApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MarvelDemoApp
+//
+//  Created by Waqas Sultan on 5/20/18.
+//  Copyright © 2018 Waqas Sultan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
